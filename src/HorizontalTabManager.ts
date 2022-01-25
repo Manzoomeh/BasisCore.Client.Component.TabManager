660f866309b18c8e48a31d4ae39be819d5a11aa8
Qadireh Moradi <kmg.d117@gmail.com>
2022-01-25 23:59:11 +0330

    add autoRun to horizontal

--- src/HorizontalTabManager.ts
+++ src/HorizontalTabManager.ts
@@ -2,6 +2,7 @@
 import "./asset/hStyle.css"
 import TabComponent from "./TabManager"
 import IUserDefineComponent from "../src/basiscore/IUserDefineComponent";
+declare const $bc: any;
 export default class HorizontalTabManager extends TabComponent{
 	constructor(owner: IUserDefineComponent){
 		super(owner,"bc-tab-container");
@@ -59,7 +60,11 @@ export default class HorizontalTabManager extends TabComponent{
 		span.addEventListener("click", (e) => {
 		  const headerElement = e.target as HTMLInputElement;
 		  const headerId = headerElement.getAttribute("data-id"); 
-		  
+		  if(this.runType == false && span.getAttribute("bc-tab-run") == null){
+			const activeOneTab = this.bodyWrapper.querySelector(`[component-id="${headerId}"]`).querySelector("basis")
+			this.initializeComponent(activeOneTab, parseInt(headerId) ,true , true);
+			span.setAttribute("bc-tab-run" , "")
+		  }
 		  this.tabNodes.map((x) => {
 			const componentId = x.getAttribute("component-id");
 			if (parseInt(headerId) == parseInt(componentId)) {
@@ -75,5 +80,46 @@ export default class HorizontalTabManager extends TabComponent{
 		  });
 		});  
 		return header;
+	  }
+	  async initializeComponent(
+		activeComponent: Element,
+		id: number,
+		runFlag : boolean,
+		runOnClick: boolean = false
+	  ): Promise<void> {
+		  const paramsInput = activeComponent.getAttribute("params")
+		if(runOnClick && runFlag){
+			if(paramsInput != "undefined"){
+				const params =JSON.parse(paramsInput) 
+				for (var key in params) {
+					await $bc.setSource(key,params[key]);
+				  }
+			  }
+		  await this.owner.processNodesAsync([activeComponent]);
+		  
+		  return
+		}
+		let componentWrapper = document.createElement("div");
+		componentWrapper.appendChild(activeComponent);
+		componentWrapper.setAttribute("bc-tab-component-wrapper", "");
+		componentWrapper.setAttribute("component-id", id.toString());
+		this.bodyWrapper.appendChild(componentWrapper);
+		this.tabNodes.push(componentWrapper);
+		this.activeComponent = componentWrapper;
+		this.tabNodes.map((x) => {
+		  x.setAttribute("bc-tab-active-component", "false");
+		});
+		this.activeComponent.setAttribute("bc-tab-active-component", "true");        
+		if(runFlag){
+			if(paramsInput != "undefined"){
+				const params =JSON.parse(paramsInput) 
+				for (var key in params) {
+					await $bc.setSource(key,params[key]);
+				  }
+			  }
+		  await this.owner.processNodesAsync([activeComponent]);
+		  
+		}
+		
 	  }
 }
